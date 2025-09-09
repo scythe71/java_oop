295f62cb99df4aa0a0615c03caab9912133e9c19
Ramma <ammarshiddiq2@gmail.com>
2025-09-09 10:51:23 +0700

    Java Day-1

---
+++ DAY_1/lib/src/latihan.java
@@ -0,0 +1,32 @@
+public class latihan {
+
+    public static void main(String[] args) {
+        String nama = "Ammar";
+        String alamat = "Buket Rata";
+        String tpt_lahir = "Banda Aceh";
+        int tgl = 17;
+        String bulan = "April";
+        int tahun = 2005;
+        String hobi = "Main HP";
+        String fav_warna = "Biru";
+        int nilai = 100;
+        float ipk = 3.6f;
+        boolean logika = true;
+        char karakter = 'A';
+
+        System.out.println("BIODATA ANGGOTA");
+        System.out.println("_______________");
+        System.out.println("NAMA                  : " + nama);
+        System.out.println("ALAMAT                : " + alamat);
+        System.out.println("TEMPAT LAHIR          : " + tpt_lahir);
+        System.out.println("TANGGAL LAHIR         : " + tgl + " " + bulan + " " + tahun);
+        System.out.println("_____________________________________");
+        System.out.println("HOBI                  : " + hobi);
+        System.out.println("WARNA KESUKAAN        : " + fav_warna + "\n");
+
+        System.out.println("isi variabel nilai    = " + nilai);
+        System.out.println("isi variabel IPK      = " + ipk);
+        System.out.println("isi variabel Logika   = " + logika);
+        System.out.println("isi variabel Karakter = " + karakter);
+    }
+}
