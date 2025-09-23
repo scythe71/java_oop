7145b0f271602397d246b96d400e02b00fa2c074
Ramma <ammarshiddiq2@gmail.com>
2025-09-23 14:54:26 +0700

    DAY_4

---
+++ lib/src/DAY_3/Segi3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Segi3 {
+    public static void main(String[] args) {
+        
+        // for(int i = 0; i < 10; i++) {
+        //     for(int j = 10; j > 0; j--) {
+        //         System.out.println("*");
+        //     }
+        // }
+
+        int jml;
+
+        try(Scanner input = new Scanner(System.in)) {
+            System.out.print("Masukkan Angka: ");
+            jml = input.nextInt();
+        }
+
+        for (int i = 1; i <= jml; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*  ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
