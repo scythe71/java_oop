4480c16e45dbf92f4daad0ab9bfbc3046a37ebee
Ramma <ammarshiddiq2@gmail.com>
2025-09-29 17:35:50 +0700

    My Day

---
+++ lib/src/DAY_4/run.java
@@ -0,0 +1,8 @@
+public class run {
+    public static void main(String[] args) {
+        test test1 = new test();
+        test1.nama = "Ammar";
+        test1.usia = 21;
+        test1.sapa();
+    }
+}
---
+++ lib/src/DAY_4/test.java
@@ -0,0 +1,8 @@
+public class test {
+    String nama;
+    int usia;
+
+    void sapa() {
+        System.out.println("Halo, saya " + nama + " umur saya " + usia);
+    }
+}
