82455db4a10999584735bfb2afd02c07561c93c4
Ramma <ammarshiddiq2@gmail.com>
2025-10-14 14:56:11 +0700

    NETBEANS FIRST

--- PAgre_Prodi/src/pagre_prodi/Jurusan.java
+++ PAgre_Prodi/src/pagre_prodi/Jurusan.java
@@ -1,4 +1,29 @@
 package pagre_prodi;
 public class Jurusan {
+    private String kode, nama;
+    private Mahasiswa daftarMhs[] = new Mahasiswa[11];
+    private int jmlMhs;
     
+    public Jurusan(String kode, String nama) {
+        this.kode = kode;
+        this.nama = nama;
+    }
+    public String getKode() {
+        return this.kode;
+    }
+    public String getNama() {
+        return this.nama;
+    }
+    public int getJmlMhs() {
+        return this.jmlMhs;
+    }
+    public void addMhs(Mahasiswa m) {
+        if(jmlMhs < this.daftarMhs.length) {
+            daftarMhs[jmlMhs] = m;
+            jmlMhs++;
+        }
+    }
+    public Mahasiswa[] getDaftarMhs() {
+        return this.daftarMhs;
+    }
 }
--- PAgre_Prodi/src/pagre_prodi/PAgre_Prodi.java
+++ PAgre_Prodi/src/pagre_prodi/PAgre_Prodi.java
@@ -1,7 +1,27 @@
 package pagre_prodi;
 public class PAgre_Prodi {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Jurusan j = new Jurusan("Teknologi Informasi dan komputer", "Politeknik Negeri Lhokseumawe");
+        Mahasiswa m1 = new Mahasiswa();
+        m1.setNim("2024573010129");
+        m1.setNama("Ammar");
+        
+        Mahasiswa m2 = new Mahasiswa();
+        m2.setNim("2024543010038");
+        m2.setNama("Afdal");
+        
+        j.addMhs(m1);
+        j.addMhs(m2);
+        
+        System.out.println("Kode Jurusan: " + j.getKode());
+        System.out.println("Nama Kampus: " + j.getNama());
+        System.out.println("List Daftar Mahasiswa: ");
+        Mahasiswa[] daftar = j.getDaftarMhs();
+        for (Mahasiswa m:daftar) {
+            if(m != null) {
+                System.out.println(" -" + m.GetNim() + " " + m.GetNama());
+            }
+        }
     }
     
 }
